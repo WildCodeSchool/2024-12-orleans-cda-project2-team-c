8cf97562202eead3b9d1eade90f5773329586827
Teddy Aboim <teddy.aboim@gmail.com>
2025-01-24 13:44:08 +0100

    add changes asked for PR

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -3,7 +3,7 @@ import '../css/loader.css';
 export default function Loader() {
   return (
     <div className='loader' aria-hidden='true'>
-      <img src={`../public/images/loader-${Math.floor(Math.random() * 5 + 1)}.png`} alt='' />
+      <img src={`/images/loader-${Math.floor(Math.random() * 5 + 1)}.png`} alt='' />
     </div>
   );
 }
