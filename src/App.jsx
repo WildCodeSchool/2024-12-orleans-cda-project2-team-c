04892ba9eca840bc388cd1f631f627618fe78763
Teddy Aboim <teddy.aboim@gmail.com>
2025-01-24 12:21:16 +0100

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+
 import './App.css';
 import Footer from './components/footer';
+import Loader from './components/loader';
 
 function App() {
+  const [data, setData] = useState(null);
+
   return (
     <>
+      {data ? 'main 🤚' : <Loader />}
       <Footer />
     </>
   );
