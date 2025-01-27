d055f27a39192a4ae9ae1805a35e8a576c263401
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-27 09:33:56 +0100

    pokemon stats

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,18 +1,18 @@
 import pokemon from '../mock-pokemon';
 import './stats.css';
 
-function StatBar({ stats, key }) {
+function StatBar({ stats }) {
   const statBarElem = [
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
-    <div></div>,
+    <div key={1}></div>,
+    <div key={2}></div>,
+    <div key={3}></div>,
+    <div key={4}></div>,
+    <div key={5}></div>,
+    <div key={6}></div>,
+    <div key={7}></div>,
+    <div key={8}></div>,
+    <div key={9}></div>,
+    <div key={10}></div>,
   ];
 
   const nbGreen = Math.round(stats.base_stat / 10);
