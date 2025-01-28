17f2efeb0530152f6b711cd8019928b1515964dd
YahyaBlm <yahya.belmou@gmail.com>
2025-01-28 22:40:24 +0100

    search-section types request

--- src/components/search-section.jsx
+++ src/components/search-section.jsx
@@ -1,30 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import '../css/search-section.css';
 
 export default function Search({ title }) {
   const [searchBy, setSearchBy] = useState('name');
+  const [types, setTypes] = useState([]);
 
-  const handleRadioChange = (event) => {
+  const handelRadio = (event) => {
     setSearchBy(event.target.value);
   };
 
+  useEffect(() => {
+    const fetchTypes = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/type/');
+        const data = await response.json();
+        setTypes(data.results);
+      } catch (error) {
+        console.error('Error while fetching types from API', error);
+      }
+    };
+
+    fetchTypes();
+  }, []);
+
   return (
     <div className='search-container'>
-      <h2>{title}</h2>
-      <div className='search-item-container'>
+      <h1>{title}</h1>
+      <form className='search-item-container'>
         <p>Search by</p>
-        <label>
-          <input
-            className='radio'
-            type='radio'
-            name='searchBy'
-            value='type'
-            checked={searchBy === 'type'}
-            onChange={handleRadioChange}
-          />
-          Type
-        </label>
+
         <label>
           <input
             className='radio'
@@ -32,12 +37,35 @@ export default function Search({ title }) {
             name='searchBy'
             value='name'
             checked={searchBy === 'name'}
-            onChange={handleRadioChange}
+            onChange={handelRadio}
           />
           Name
         </label>
-        {searchBy === 'type' ? <select>{}</select> : <input type='text' placeholder='Enter a Pokémon name' />}
-      </div>
+
+        <label>
+          <input
+            className='radio'
+            type='radio'
+            name='searchBy'
+            value='type'
+            checked={searchBy === 'type'}
+            onChange={handelRadio}
+          />
+          Type
+        </label>
+
+        {searchBy === 'type' ? (
+          <select>
+            {types.map((type) => (
+              <option key={type.id} value={type.name}>
+                {type.name.charAt(0).toUpperCase() + type.name.slice(1)}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input type='text' placeholder='Enter a Pokémon name' />
+        )}
+      </form>
     </div>
   );
 }
