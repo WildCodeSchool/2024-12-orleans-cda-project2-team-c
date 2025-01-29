a4b02f643991b78f793a97ee08cb2b4e3cba2301
TeddyAgt <152014673+TeddyAgt@users.noreply.github.com>
2025-01-29 09:40:47 +0100

    refactore search-section.jsx

--- src/components/search-section.jsx
+++ src/components/search-section.jsx
@@ -27,7 +27,7 @@ export default function Search({ title }) {
   return (
     <div className='search-container'>
       <h1>{title}</h1>
-      <form className='search-item-container' aria-role='search'>
+      <form className='search-item-container' role='search'>
         <p>Search by</p>
 
         <label>
