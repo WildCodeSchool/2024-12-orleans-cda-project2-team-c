51a0f2eab275eaffc421548a9536cffd16b3a2a2
Teddy Aboim <teddy.aboim@gmail.com>
2025-02-02 12:41:23 +0100

    add responsive (4 lines LMAO)

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,7 +1,23 @@
+import demoPicture from '../assets/images/114.png';
+import Button from '../components/button';
+import '../css/quiz.css';
+
 export default function Quiz() {
+  function handleClickStartBtn() {}
   return (
-    <>
-      <h3>Quiz page !</h3>
-    </>
+    <section className='quiz-section'>
+      <h1>Quiz</h1>
+      <div className='pictures-container'>
+        <img src={demoPicture} alt='' className='demo-picture demo-picture--hidden' />
+        <img src={demoPicture} alt='' className='demo-picture' />
+      </div>
+      <p>
+        Find out to which Pokémon the figure belongs to and try to make a perfect score ! Every Pokémon you discover
+        will be added to your personal Pokédex! Go Catch’ em all !
+      </p>
+      <Button onClick={handleClickStartBtn} className='button--red'>
+        Start !
+      </Button>
+    </section>
   );
 }
