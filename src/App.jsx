217cdb5b78953a4da3eeefb060916fcd1c957fc9
YahyaBlm <yahya.belmou@gmail.com>
2025-02-05 12:19:33 +0100

    results display

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from './components/footer';
 import Header from './components/header';
-import QuizResults from './components/quiz-results';
 
 function App() {
   return (
     <>
       <Header />
-      <QuizResults />
-      {/* <main>
+      <main>
         <Outlet />
-      </main> */}
+      </main>
       <Footer />
     </>
   );
