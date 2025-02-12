04ddbc2d083764d5379de320044d1d7cf49a1b10
Teddy Aboim <teddy.aboim@gmail.com>
2025-02-12 20:13:29 +0100

    add legends array

---
+++ src/models/legends.js
@@ -0,0 +1,117 @@
+export default [
+  {
+    id: 1026,
+    name: 'Charles',
+    abilities: [{}, {}],
+    cries: { latest: '' },
+    height: 7,
+    moves: [],
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: '',
+        },
+      },
+    },
+    stats: [],
+  },
+  {
+    id: 1027,
+    name: 'Yahya',
+    abilities: [{}, {}],
+    cries: { latest: '' },
+    height: 7,
+    moves: [],
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: '',
+        },
+      },
+    },
+    stats: [],
+  },
+  {
+    id: 1028,
+    name: 'Teddy',
+    abilities: [
+      {
+        ability: {
+          name: '',
+        },
+      },
+      {
+        ability: {
+          name: '',
+        },
+      },
+    ],
+    cries: { latest: '' },
+    height: 7,
+    moves: [
+      {
+        move: {
+          name: 'fix bug',
+        },
+      },
+    ],
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: '',
+        },
+      },
+    },
+    stats: [
+      {
+        base_stat: 45,
+        effort: 0,
+        stat: {
+          name: 'hp',
+          url: 'https://pokeapi.co/api/v2/stat/1/',
+        },
+      },
+      {
+        base_stat: 100,
+        effort: 0,
+        stat: {
+          name: 'attack',
+          url: 'https://pokeapi.co/api/v2/stat/2/',
+        },
+      },
+      {
+        base_stat: 12,
+        effort: 0,
+        stat: {
+          name: 'defense',
+          url: 'https://pokeapi.co/api/v2/stat/3/',
+        },
+      },
+      {
+        base_stat: 120,
+        effort: 1,
+        stat: {
+          name: 'special-attack',
+          url: 'https://pokeapi.co/api/v2/stat/4/',
+        },
+      },
+      {
+        base_stat: 20,
+        effort: 0,
+        stat: {
+          name: 'special-defense',
+          url: 'https://pokeapi.co/api/v2/stat/5/',
+        },
+      },
+      {
+        base_stat: 92,
+        effort: 0,
+        stat: {
+          name: 'speed',
+          url: 'https://pokeapi.co/api/v2/stat/6/',
+        },
+      },
+    ],
+    weight: 58,
+  },
+];
