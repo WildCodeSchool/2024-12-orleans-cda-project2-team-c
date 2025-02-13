0c2fc94b8fd342b501c3c7ff7eed1dc97f52324c
Teddy Aboim <teddy.aboim@gmail.com>
2025-02-13 15:03:45 +0100

    change wallpaper on quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import Footer from './components/footer';
 import Header from './components/header';
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <>
       <Header />
-      <main>
+      <main className={pathname === '/quiz' ? 'main-bg--2' : 'main-bg--1'}>
         <Outlet />
       </main>
       <Footer />
