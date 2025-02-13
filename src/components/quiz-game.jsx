153c842ded5398bea47906a89200b07032186c34
Teddy Aboim <teddy.aboim@gmail.com>
2025-02-13 11:58:38 +0100

    fix button color bug

--- src/components/quiz-game.jsx
+++ src/components/quiz-game.jsx
@@ -163,15 +163,17 @@ export default function QuizGame({ game, setHasFinished }) {
             <Button
               key={answer.id}
               className={`capital ${
-                timerIsRunning
-                  ? clickedButton
-                    ? clickedButton === answer.id
-                      ? isAnswerRight
-                        ? 'button--success'
-                        : 'button--red'
-                      : 'button--disabled'
-                    : 'button--yellow'
+                // timerIsRunning && !clickedButton ?
+                clickedButton
+                  ? clickedButton === answer.id
+                    ? isAnswerRight
+                      ? 'button--success'
+                      : 'button--red'
+                    : 'button--disabled'
+                  : timerIsRunning
+                  ? 'button--yellow'
                   : 'button--disabled'
+                // : 'button--disabled'
               }`}
               onClick={() => endRound(answer.id)}
               disabled={clickedButton || !timerIsRunning}
