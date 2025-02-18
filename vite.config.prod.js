edb8fb298a42e18a4b290112a4c4c5f1f3ab8e8b
TeddyAgt <152014673+TeddyAgt@users.noreply.github.com>
2025-02-18 18:35:36 +0100

    fix typo

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2024-12-orleans-cda-projet2-team-c/',
+  base: '/2024-12-orleans-cda-project2-team-c/',
   plugins: [react()],
 });
